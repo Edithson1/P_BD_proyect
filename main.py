ea5c548399fe7547176b9fc12b964d81d9c15f4a
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-10-15 11:31:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 import pymysql
+import os
+
 
 app = Flask(__name__)
 
@@ -53,3 +55,8 @@ def actualizar_registro():
         return jsonify({"error": str(e)})
     finally:
         connection.close()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto definido por Railway o por defecto 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
+

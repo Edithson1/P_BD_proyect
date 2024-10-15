21096b952ee1318e6872be1028579dd35abf8357
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-10-15 11:33:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,3 @@ def actualizar_registro():
         return jsonify({"error": str(e)})
     finally:
         connection.close()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Usa el puerto definido por Railway o por defecto 5000
-    app.run(debug=True, host='0.0.0.0', port=port)
-

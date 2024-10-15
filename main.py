ed712679f8b6d0749da76dd0167ad1dde8706bf6
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-10-15 11:14:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ def actualizar_registro():
         connection.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)

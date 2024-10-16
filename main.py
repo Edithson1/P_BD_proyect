8a15d9151a0868c11b5bd0fc051c4b6e9b69b704
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-10-15 23:48:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,58 +3,6 @@ import pymysql
 
 app = Flask(__name__)
 
-# Función para conectarse a la base de datos
-def get_db_connection():
-    return pymysql.connect(
-        host='autorack.proxy.rlwy.net',
-        port=56092,
-        user='all_user',
-        password='8765',
-        database='bd_project'
-    )
-
-# Ruta para obtener el último movimiento desde la base de datos
-@app.route('/movimientos', methods=['GET'])
-def obtener_movimientos():
-    connection = get_db_connection()
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute('''
-                SELECT id_historial, time_insert, nivel, orientation, posicion_x, posicion_y
-                FROM historial
-                ORDER BY id_historial DESC
-                LIMIT 1
-            ''')
-            movimiento = cursor.fetchone()
-            return jsonify({
-                'id_historial': movimiento[0],
-                'time_insert': movimiento[1],
-                'nivel': movimiento[2],
-                'orientation': movimiento[3],
-                'posicion_x': movimiento[4],
-                'posicion_y': movimiento[5]
-            })
-    except pymysql.MySQLError as e:
-        return jsonify({"error": str(e)})
-    finally:
-        connection.close()
-
-# Ruta para actualizar el registro en la base de datos
-@app.route('/actualizar', methods=['POST'])
-def actualizar_registro():
-    data = request.json
-    connection = get_db_connection()
-    try:
-        with connection.cursor() as cursor:
-            cursor.callproc('registrar_voto', (data['voto'], data['historial_id'], data['nivel'], data['orientation'], data['posicion_x'], data['posicion_y']))
-            connection.commit()
-            return jsonify({"status": "success"})
-    except pymysql.MySQLError as e:
-        return jsonify({"error": str(e)})
-    finally:
-        connection.close()
-
-
 @app.route('/')
 def hello_world():
     return 'Hello from Flask with SQLAlchemy!'
